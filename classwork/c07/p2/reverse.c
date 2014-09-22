d8555a727b9bbd02937801977c172fabf1179c0f
allenyin <allen.s.yin@gmail.com>
2014-09-22 01:11:57 -0400

    All the reverse files.

--- classwork/c07/p2/reverse.c
+++ classwork/c07/p2/reverse.c
@@ -2,32 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
-void reverse(char * str) {
-    int len = strlen(str);
-    char * end = str+len-1;
-    char temp;
-
-    while (end > str) {
-        // save the *str in temp
-        temp = *str;
-        // replace *str with *end
-        *str = *end;
-        // put str to the end
-        *end = temp;
-        // increment pointers
-        end--;
-        str++;
+void printline(char buffer[], int size) {
+    for (int i=0; i<size; i++) {
+        printf("%c", buffer[i]);
     }
+    printf("\n");
 }
 
-int main(int argc, char ** argv) {
-    if (argc == 1) {
-        // nothing is entered for us to reverse
-        return EXIT_SUCCESS;
+void reverse(char buffer[], int size) {
+    if (size <= 1) {
+        return;
+    }
+    char temp;
+    temp = buffer[0];
+    buffer[0] = buffer[size-1];
+    buffer[size-1] = temp;
+    reverse(buffer+1, size-2);
+}
+
+void reverse_helper(char buffer[]) {
+    int len = strlen(buffer);
+    if (buffer[len-1] == '\n') {
+        reverse(buffer, len-1);
+        printline(buffer, len-1);
     }
     else {
-        char buffer[500] = 
-    
+        reverse(buffer, len);
+        printline(buffer, len);
+    }
+    return;
+}
 
+int main(void) {
+    /* Program behavior:
+     * After starting the program via "./reverse", the user keeps entering
+     * input. The program stops taking inputs when EOF or "/n" is detected.
+     * Then the reverse of the user input is outputted, and the program exits.
+     */
+     char buffer[10];
+     while ( (fgets(buffer, 10, stdin)!= NULL)) {
+         
+         reverse_helper(buffer);
+     }
     return EXIT_SUCCESS;
 }
